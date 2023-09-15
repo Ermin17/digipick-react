1e68eeb3ce31d4d803a2e93a51fee52b426b86ec
Ermin Sljivo <esljivo1996@gmail.com>
2023-09-15 15:49:29 -0400

    Commit app, main, and styles files

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Lock from './components/Lock.js';
+import './styles.css';
+
+const App = () => {
+  return (
+    <>
+    {/* <div className='circle'>Hello world!</div> */}
+    <div>
+      <Lock />
+    </div>
+    </>
+  )
+};
+
+export default App;
