9bcc84db3724bb60311ccef118d3a5884e1d42bc
Ermin Sljivo <esljivo1996@gmail.com>
2023-09-19 15:28:18 -0400

    finish function to generate remaining random array

---
+++ src/helpers/generateRemainingLockpicks.js
@@ -0,0 +1,45 @@
+// This will generate purely random lockpicks for the remaining spots
+const generateRemainingLockpicks = (difficulty) => {
+  // Limit the remaining lockpicks to total - number of lockpicks set by difficulty
+  // Ex: Novice has 2 solutions but 4 total lockpicks so generate 2 remaining lockpicks
+  var numberOfSolutions;
+  var randomSolutions = [];
+  var maxZeros = 4;
+  var zerosCount = 0;
+  var solutionCount = 0;
+
+  if (difficulty === 'novice') {
+    numberOfSolutions = 2;
+  } else if (difficulty === 'advanced') {
+    numberOfSolutions = 4;
+  } else if (difficulty === 'expert') {
+    numberOfSolutions = 6;
+  } else if (difficulty === 'master') {
+    numberOfSolutions = 8;
+  }
+
+  while (solutionCount < numberOfSolutions) {
+    var newLockpick = [];
+    for (var i = 0; i < 20; i++) {
+      if (zerosCount < maxZeros && Math.random() > 0.5) {
+        newLockpick.push(0);
+        zerosCount++;
+      } else {
+        newLockpick.push(1);
+      }
+
+    }
+
+    // Ensure there is at least 1 zero
+    var randomIndex = Math.floor(Math.random() * 20);
+    newLockpick[randomIndex] = 0;
+    randomSolutions.push(newLockpick);
+    solutionCount++;
+
+  }
+
+  return randomSolutions;
+
+};
+
+module.exports = generateRemainingLockpicks;
