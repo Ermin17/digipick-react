c4a88cf828a6c59a1a13db630c9dcbbd0a343677
Ermin Sljivo <esljivo1996@gmail.com>
2023-09-23 13:53:19 -0400

    add visual for selected lockpick

--- src/components/Lockpick.js
+++ src/components/Lockpick.js
@@ -16,21 +16,42 @@ const Lockpick = ({ lockpick }) => {
 
   const stepAngle = (2 * Math.PI) / lockpick.length; // Angle between each step
 
-  const pathData = lockpick.map((value, index) => {
-    if (value === 1) {
-      const radius = maxRadius; // Use the same radius for each circle
-      const startAngle = stepAngle * index;
-      const endAngle = stepAngle * (index + 1);
-      const x1 = centerX + radius * Math.cos(startAngle);
-      const y1 = centerY + radius * Math.sin(startAngle);
-      const x2 = centerX + radius * Math.cos(endAngle);
-      const y2 = centerY + radius * Math.sin(endAngle);
+  const paths = lockpick.map((value, index) => {
 
-      return `M ${x1},${y1} A ${radius},${radius} 0 0,1 ${x2},${y2}`;
+    const strokeWidth = value === 1 ? '8' : '1'; // Set the stroke width based on value
+
+    const radius = maxRadius; // Use the same radius for each circle
+    const startAngle = stepAngle * index;
+    const endAngle = stepAngle * (index + 1);
+
+    const x1 = centerX + radius * Math.cos(startAngle);
+    const y1 = centerY + radius * Math.sin(startAngle);
+    const x2 = centerX + radius * Math.cos(endAngle);
+    const y2 = centerY + radius * Math.sin(endAngle);
+
+    if (value === 1) {
+      return (
+        <path
+          key={index}
+          d={`M ${x1},${y1} A ${radius},${radius} 0 0,1 ${x2},${y2}`}
+          fill='none'
+          stroke='aliceblue'
+          strokeWidth={strokeWidth} // Use the calculated stroke width
+        />
+      );
     } else {
-      return null;
+      return (
+        <path
+          key={index}
+          d={`M ${x1},${y1} A ${radius},${radius} 0 0,1 ${x2},${y2}`}
+          fill='none'
+          stroke='aliceblue'
+          strokeWidth={strokeWidth} // Use the calculated stroke width
+        />
+      );
     }
-  }).join(` M ${centerX},${centerY} m ${-gapSize},${-gapSize}`);
+
+  });
 
   const svgWidth = maxRadius * 2 + gapSize * 30; // Calculate the SVG width
   const svgHeight = maxRadius * 2 + gapSize * 30; // Calculate the SVG height
@@ -43,12 +64,10 @@ const Lockpick = ({ lockpick }) => {
           (svgWidth - (maxRadius * 2 + gapSize * 2)) / 2
         }, ${(svgHeight - (maxRadius * 2 + gapSize * 2)) / 2})`}
       >
-        <path
-          d={pathData}
-          fill='none'
-          stroke='aliceblue'
-          strokeWidth='8'
-        />
+        {paths}
+        {lockpick === selectedLockpick && (
+          <circle cx={centerX} cy={centerY} r={maxRadius / 2} fill='aliceblue' />
+        )}
       </g>
     </svg>
   );
